537a1fda0f7a6ab04f03c17419291022a126ee1e
NAvi349 <66086031+NAvi349@users.noreply.github.com>
2022-04-01 07:23:45 +0530

    Update Assembly.s

--- instruction memory/Assembly.s
+++ instruction memory/Assembly.s
@@ -1,25 +1,14 @@
-main: 	addi x1, x0, 15	
-		addi x2, x0, 22 	
-		add x3, x1, x2		
-		or x4, x3, x2 		
-		xor x5, x3, x4 		
-		add x6, x5, x4 		
-		beq x5, x6, end 	
-		slt x4, x3, x4 		
-		beq x4, x0, around 
-		addi x6, x0, 0 	
+main: 		addi x3, x0, 10 		# x3 = 10 
+		addi x4, x0, 12			# x4 = 12
+		or x5, x3, x4 			# x5 = (10 OR 12) = 14 
+		and x6, x3, x4 			# x5 = (10 AND 12) = 8		
+		bne x5, x6, end 		# should be taken 
+		addi x4, x4, x5 		# shouldn't be executed
 		
-around: slt x4, x5, x3 		
-		add x6, x4, x5 		
-		sub x2, x3, x1		
-		sw x1, 84(x3) 		
-		lw x1, 84(x3) 	
-		addi x1, x1, 0	
-		add x7, x2, x3 		
-		jal x3, end 		
+around: slt x7, x5, x6 			# x7 = (8 < 14) = 1		
+		sub x8, x6, x5 			# x8 = (8 - 14) = -6 		
+		sw x4, 10(x3) 			#[20] = 12 
+		lw x9, 10(x3) 			# x9 = [20] = 12	
+		xor x10, x2, x2			# x10 = x2 ^ x2
 		
-		
-end: 	add x2, x2, x9 		
-		addi x4, x0, 1
-				
-done: 	beq x1, x1, done 	
+done: 	beq x2, x2, done 		#infinite loop
